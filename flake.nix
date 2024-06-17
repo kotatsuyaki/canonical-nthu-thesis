1e0edf9e715148bf47f5c4b722ba86b583c09d2d
kotatsuyaki <kotatsuyaki@mail.kotatsu.dev>
2024-06-17 10:01:08 +0800

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            typst
+          ];
+          TYPST_FONT_PATHS = "${pkgs.ttf-tw-moe}";
+        };
+      });
+}
